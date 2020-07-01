e71de36613517c77c5fafc6b6ab6e0717340a1a2
Kristo Kiis <kristo@onoffapp.com>
2020-07-02 00:04:50 +0300

    Initial Commit

---
+++ LeFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LeFood
+//
+//  Created by Kristo Kiis on 02.07.2020.
+//  Copyright © 2020 BDCApps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
